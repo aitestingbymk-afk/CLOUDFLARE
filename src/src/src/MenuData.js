457a67ae0f4f49a719c6f74a336761d713390eba
aitestingbymk-afk <aitestingbymk@gmail.com>
2025-09-02 00:39:53 +0530

    Create MenuData.js

---
+++ src/src/src/MenuData.js
@@ -0,0 +1,38 @@
+const menu = {
+  pizzas: [
+    { name: "Onion Pizza", sizes: { Small: 79, Medium: 149, Large: 299 } },
+    { name: "Onion & Capsicum Pizza", sizes: { Small: 79, Medium: 149, Large: 299 } },
+    { name: "Corn with Cheese Pizza", sizes: { Small: 89, Medium: 169, Large: 299 } },
+    { name: "Margherita Pizza", sizes: { Small: 99, Medium: 179, Large: 299 } },
+    { name: "Tandoori Paneer Pizza", sizes: { Small: 109, Medium: 159, Large: 229 } },
+    { name: "BaggetMai Special Pizza", sizes: { Small: 109, Medium: 199, Large: 349 } },
+  ],
+  burgers: [
+    { name: "Veg Aloo Burger", price: 59 },
+    { name: "Crispy Veg Burger", price: 79 },
+    { name: "Veg Cheese Burger", price: 89 },
+    { name: "Spicy Paneer Burger", price: 109 },
+  ],
+  sandwiches: [
+    { name: "2 Grill Sandwich Lite", price: 89 },
+    { name: "2 Grill Sandwich", price: 95 },
+    { name: "2 Paneer Sandwich", price: 99 },
+    { name: "2 Hozzarella", price: 89 },
+  ],
+  wraps: [
+    { name: "2 Aloo Wrap", price: 99 },
+    { name: "Saucy Wrap", price: 119 },
+    { name: "Paneer Wrap", price: 99 },
+  ],
+  fries: [
+    { name: "French Fries", price: 59 },
+    { name: "Piri-piri Fries", price: 69 },
+    { name: "Loaded Fries", price: 89 },
+  ],
+  addons: [
+    { name: "Extra Cheese", price: 30 },
+    { name: "Add Cheese (Burger)", price: 9 }
+  ]
+};
+
+export default menu;
